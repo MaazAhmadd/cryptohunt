07c2ad423f78b191e603c0a2293c392cb6908ad5
Maaz <maaz.ahmad21399@gmail.com>
2021-08-03 19:24:25 +0500

    sfds

--- src/components/RightNav.js
+++ src/components/RightNav.js
@@ -34,34 +34,36 @@ const RightNav = ({ open }) => {
     localStorage.getItem("logged_in") == 1 ? false : true;
 
   return (
-    <Ul open={open} className="header_navbar_right_inner">
-      <li className="header_item">
-        <Link to="/add_coin" className="header_link">
-          Add Coin
-        </Link>
-      </li>
-      <li className="header_item">
-        <Link to="/promote" className="header_link">
-          Promote
-        </Link>
-      </li>
-      <li className="header_item">
-        <Link to="/newsletter" className="header_link">
-          Newsletter
-        </Link>
-      </li>
-      <li className="header_item">
-        {is_user_logged_in ? (
-          <Link to="/login" className="header_link">
-            Sign In
+    <div className="nav_outer">
+      <Ul open={open} className="header_navbar_right_inner">
+        <li className="header_item">
+          <Link to="/add_coin" className="header_link">
+            Add Coin
           </Link>
-        ) : (
-          <Link to="/logout" className="header_link">
-            Sign Out
+        </li>
+        <li className="header_item">
+          <Link to="/promote" className="header_link">
+            Promote
           </Link>
-        )}
-      </li>
-    </Ul>
+        </li>
+        <li className="header_item">
+          <Link to="/newsletter" className="header_link">
+            Newsletter
+          </Link>
+        </li>
+        <li className="header_item">
+          {is_user_logged_in ? (
+            <Link to="/login" className="header_link">
+              Sign In
+            </Link>
+          ) : (
+            <Link to="/logout" className="header_link">
+              Sign Out
+            </Link>
+          )}
+        </li>
+      </Ul>
+    </div>
   );
 };
 

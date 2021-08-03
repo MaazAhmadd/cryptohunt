4bfd822e944e0daae7aa9bfdb7baeed41e9bf24c
Maaz <maaz.ahmad21399@gmail.com>
2021-08-03 19:16:09 +0500

    navbar burger added

---
+++ src/components/RightNav.js
@@ -0,0 +1,67 @@
+import React from "react";
+import styled from "styled-components";
+
+const Ul = styled.ul`
+  list-style: none;
+  display: flex;
+  flex-flow: row nowrap;
+
+  li {
+    padding: 18px 10px;
+  }
+
+  @media (max-width: 768px) {
+    flex-flow: column nowrap;
+    background-color: #0d2538;
+    position: fixed;
+    transform: ${({ open }) => (open ? "translateX(0)" : "translateX(100%)")};
+    top: 0;
+    right: 0;
+    height: 100vh;
+    width: 300px;
+    padding-top: 3.5rem;
+    transition: transform 0.3s ease-in-out;
+
+    li {
+      color: #fff;
+    }
+  }
+`;
+
+const RightNav = ({ open }) => {
+  const is_user_logged_in =
+    localStorage.getItem("logged_in") == 1 ? false : true;
+
+  return (
+    <Ul open={open} className="header_navbar_right_inner">
+      <li className="header_item">
+        <Link to="/add_coin" className="header_link">
+          Add Coin
+        </Link>
+      </li>
+      <li className="header_item">
+        <Link to="/promote" className="header_link">
+          Promote
+        </Link>
+      </li>
+      <li className="header_item">
+        <Link to="/newsletter" className="header_link">
+          Newsletter
+        </Link>
+      </li>
+      <li className="header_item">
+        {is_user_logged_in ? (
+          <Link to="/login" className="header_link">
+            Sign In
+          </Link>
+        ) : (
+          <Link to="/logout" className="header_link">
+            Sign Out
+          </Link>
+        )}
+      </li>
+    </Ul>
+  );
+};
+
+export default RightNav;
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,52 +1,73 @@
 import "../App.css";
 import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Nav = styled.nav`
+  width: 100%;
+  height: 55px;
+  border-bottom: 2px solid #f1f1f1;
+  padding: 0 20px;
+  display: flex;
+  justify-content: space-between;
+
+  .logo {
+    padding: 15px 0;
+  }
+`;
 
 function Navbar() {
-  const is_user_logged_in =
-    localStorage.getItem("logged_in") == 1 ? false : true;
-
   return (
-    <>
-      <div className="nav_outer">
-        <div className="header_logo_outer">
-          <h3 className="header_logo_inner">
-            <Link to="/">CryptoHunt</Link>
-          </h3>
-        </div>
-
-        <div className="header_navbar_right_outer">
-          <ul className="header_navbar_right_inner">
-            <li className="header_item">
-              <Link to="/add_coin" className="header_link">
-                Add Coin
-              </Link>
-            </li>
-            <li className="header_item">
-              <Link to="/promote" className="header_link">
-                Promote
-              </Link>
-            </li>
-            <li className="header_item">
-              <Link to="/newsletter" className="header_link">
-                Newsletter
-              </Link>
-            </li>
-            <li className="header_item">
-              {is_user_logged_in ? (
-                <Link to="/login" className="header_link">
-                  Sign In
-                </Link>
-              ) : (
-                <Link to="/logout" className="header_link">
-                  Sign Out
-                </Link>
-              )}
-            </li>
-          </ul>
-        </div>
+    <Nav>
+      <div className="header_logo_outer">
+        <h3 className="header_logo_inner">
+          <Link to="/">CryptoHunt</Link>
+        </h3>
       </div>
-    </>
+      <Burger />
+    </Nav>
   );
+  // (
+  //   <>
+  //     <div className="nav_outer">
+  //       <div className="header_logo_outer">
+  //         <h3 className="header_logo_inner">
+  //           <Link to="/">CryptoHunt</Link>
+  //         </h3>
+  //       </div>
+
+  //       <div className="header_navbar_right_outer">
+  //         <ul className="header_navbar_right_inner">
+  //           <li className="header_item">
+  //             <Link to="/add_coin" className="header_link">
+  //               Add Coin
+  //             </Link>
+  //           </li>
+  //           <li className="header_item">
+  //             <Link to="/promote" className="header_link">
+  //               Promote
+  //             </Link>
+  //           </li>
+  //           <li className="header_item">
+  //             <Link to="/newsletter" className="header_link">
+  //               Newsletter
+  //             </Link>
+  //           </li>
+  //           <li className="header_item">
+  //             {is_user_logged_in ? (
+  //               <Link to="/login" className="header_link">
+  //                 Sign In
+  //               </Link>
+  //             ) : (
+  //               <Link to="/logout" className="header_link">
+  //                 Sign Out
+  //               </Link>
+  //             )}
+  //           </li>
+  //         </ul>
+  //       </div>
+  //     </div>
+  //   </>
+  // );
 }
 
 export default Navbar;

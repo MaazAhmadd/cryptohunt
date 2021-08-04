b55990835f25e739edd7be06595ea81fae0ad3e5
Maaz <maaz.ahmad21399@gmail.com>
2021-08-05 00:54:55 +0500

    fsd

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -5,40 +5,28 @@ export default function Newsletter() {
     <div>
       <div id="DIV_1">
         <div id="DIV_2">
-          <div id="DIV_3">
-            <div id="DIV_4">
-              <div id="DIV_5">
-                <div id="DIV_6">
-                  <img
-                    src="https://coinhunt.cc/resources/Images/bike.svg"
-                    alt="Logo"
-                    id="IMG_7"
-                  />
-                </div>
-                <div id="DIV_8">
-                  <div id="DIV_9">
-                    <div id="DIV_10">Subscribe to our newsletter</div>
-                  </div>
-                  <div id="DIV_11">
-                    Get the best high potential coins right into your inbox.
-                  </div>
-                  <div id="DIV_12">
-                    <div id="DIV_13">
-                      <div id="DIV_14"></div>
-                      <div id="DIV_15">
-                        <input
-                          type="email"
-                          placeholder="Your email"
-                          id="INPUT_16"
-                        />
-                      </div>
-                      <button type="button" id="BUTTON_17">
-                        Submit
-                      </button>
-                    </div>
-                  </div>
-                </div>
+          <img
+            src="https://coinhunt.cc/resources/Images/bike.svg"
+            alt="Logo"
+            id="IMG_3"
+          />
+        </div>
+        <div id="DIV_4">
+          <div id="DIV_5">
+            <div id="DIV_6">Subscribe to our newsletter</div>
+          </div>
+          <div id="DIV_7">
+            Get the best high potential coins right into your inbox.
+          </div>
+          <div id="DIV_8">
+            <div id="DIV_9">
+              <div id="DIV_10"></div>
+              <div id="DIV_11">
+                <input type="email" placeholder="Your email" id="INPUT_12" />
               </div>
+              <button type="button" id="BUTTON_13">
+                Submit
+              </button>
             </div>
           </div>
         </div>

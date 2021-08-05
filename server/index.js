1f8563b8e8c7a1c859c4f18e6d71d3338c7b078e
Maaz <maaz.ahmad21399@gmail.com>
2021-08-05 14:02:35 +0500

    fds

--- server/index.js
+++ server/index.js
@@ -297,7 +297,7 @@ app.post("/approve_coin", async function (req, res) {
   connection.query(
     `select role from users where email = '${user}';`,
     function (error, results, fields) {
-      if (results.role == "admin") {
+      if (results[0].role == "admin") {
         connection.query(
           `UPDATE coin set status = 'approved' where id = ${coin_id}`,
           function (error, results, fields) {
@@ -322,7 +322,7 @@ app.post("/reject_coin", async function (req, res) {
     `select role from users where email = '${user}';`,
     function (error, results, fields) {
       console.log(results);
-      if (results.role == "admin") {
+      if (results[0].role == "admin") {
         connection.query(
           `DELETE from coin where id = ${coin_id}`,
           function (error, results, fields) {

56675c306fee48dc09280446492bc5ea45850e02
Maaz <maaz.ahmad21399@gmail.com>
2021-08-06 16:51:22 +0500

    gdf

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -87,7 +87,7 @@ export default function Newsletter() {
                           verticalAlign: "middle",
                           display: "inline-block",
                           color: "#fff",
-                          backgroundColor: "#007bff",
+                          backgroundColor: "rgb(0, 123, 255) !important",
                           borderColor: "#007bff",
                           cursor: "pointer",
                           width: "100%",

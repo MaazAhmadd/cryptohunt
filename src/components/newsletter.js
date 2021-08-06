778fe45945c604c92d954af700c8ba322b9debb6
Maaz <maaz.ahmad21399@gmail.com>
2021-08-06 16:41:16 +0500

    fsd

--- src/components/newsletter.js
+++ src/components/newsletter.js
@@ -18,81 +18,85 @@ export default function Newsletter() {
               Get the best high potential coins right into your inbox.
             </div>
             <div>
-              <div style={{ width: "80%", float: "left", marginTop: "5%" }}>
-                <div style={{ marginBlock: "1rem" }}>
-                  <input
-                    style={{
-                      font: "400 13.3px Arial",
-                      WebkitRtlOrdering: "logical",
-                      cursor: "text",
-                      textAlign: "start",
-                      appearance: "auto",
-                      letterSpacing: "normal",
-                      wordSpacing: "normal",
-                      textTransform: "none",
-                      textIndent: "0px",
-                      textShadow: "none",
-                      WebkitWritingMode: "horizontal-tb !important",
-                      textRendering: "auto",
-                      margin: "0",
-                      fontFamily: "inherit",
-                      overflow: "visible",
-                      transition:
-                        "border-color .15s ease-in-out,box-shadow .15s ease-in-out,-webkit-box-shadow .15s ease-in-out",
-                      border: "1px solid #ced4da",
-                      borderRadius: ".25rem",
-                      height: "calc(1.5em + .75rem + 2px)",
-                      padding: ".375rem .75rem",
-                      fontSize: "1rem",
-                      fontWeight: "400",
-                      lineHeight: "1.5",
-                      display: "block",
-                      background: "rgb(255, 255, 255) !important",
-                      color: "rgb(0, 0, 0) !important",
-                      width: "100%",
-                    }}
-                    type="email"
-                    placeholder="Your email"
-                    id="INPUT_12"
-                  />
-                </div>
-                <button
-                  style={{
-                    font: "400 13.3px Arial",
-                    alignItems: "flex-start",
-                    textIndent: "0px",
-                    textShadow: "none",
-                    letterSpacing: "normal",
-                    wordSpacing: "normal",
-                    WebkitWritingMode: "horizontal-tb !important",
-                    textRendering: "auto",
-                    margin: "0",
-                    fontFamily: "inherit",
-                    overflow: "visible",
-                    textTransform: "none",
-                    WebkitAppearance: "button",
-                    transition:
-                      "color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out,-webkit-box-shadow .15s ease-in-out",
-                    border: "1px solid transparent",
-                    padding: ".375rem .75rem",
-                    fontSize: "1rem",
-                    lineHeight: "1.5",
-                    borderRadius: ".25rem",
-                    textAlign: "center",
-                    verticalAlign: "middle",
-                    display: "inline-block",
-                    fontWeight: "400",
-                    color: "#fff",
-                    backgroundColor: "#007bff",
-                    borderColor: "#007bff",
-                    cursor: "pointer",
-                    width: "100%",
-                  }}
-                  type="button"
-                  id="BUTTON_13"
+              <div
+                id="mc_embed_signup"
+                style={{ width: "80%", float: "left", marginTop: "5%" }}
+              >
+                <form
+                  action="https://con.us5.list-manage.com/subscribe/post?u=ecfebd4ed89f081787b30a7ea&id=afc6e39fd9"
+                  method="post"
+                  id="mc-embedded-subscribe-form"
+                  name="mc-embedded-subscribe-form"
+                  className="validate"
+                  target="_blank"
+                  noValidate
                 >
-                  Submit
-                </button>
+                  <div id="mc_embed_signup_scroll">
+                    <div style={{ marginBlock: "1rem" }}>
+                      <input
+                        type="email"
+                        defaultValue
+                        name="EMAIL"
+                        className="email"
+                        id="mce-EMAIL"
+                        placeholder="email address"
+                        required
+                        style={{
+                          fontFamily: "inherit",
+                          transition:
+                            "border-color .15s ease-in-out,box-shadow .15s ease-in-out,-webkit-box-shadow .15s ease-in-out",
+                          border: "1px solid #ced4da",
+                          borderRadius: ".25rem",
+                          height: "calc(1.5em + .75rem + 2px)",
+                          padding: ".375rem .75rem",
+                          fontSize: "1rem",
+                          display: "block",
+                          width: "100%",
+                        }}
+                      />
+                    </div>
+                    {/* real people should not fill this in and expect good things - do not remove this or risk form bot signups*/}
+                    <div
+                      style={{ position: "absolute", left: "-5000px" }}
+                      aria-hidden="true"
+                    >
+                      <input
+                        type="text"
+                        name="b_ecfebd4ed89f081787b30a7ea_afc6e39fd9"
+                        tabIndex={-1}
+                        defaultValue
+                      />
+                    </div>
+                    <div className="clear">
+                      <input
+                        type="submit"
+                        defaultValue="Subscribe"
+                        name="subscribe"
+                        id="mc-embedded-subscribe"
+                        className="button"
+                        style={{
+                          fontFamily: "inherit",
+                          overflow: "visible",
+                          transition:
+                            "color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out,-webkit-box-shadow .15s ease-in-out",
+                          border: "1px solid transparent",
+                          padding: ".375rem .75rem",
+                          fontSize: "1rem",
+                          lineHeight: "1.5",
+                          borderRadius: ".25rem",
+                          textAlign: "center",
+                          verticalAlign: "middle",
+                          display: "inline-block",
+                          color: "#fff",
+                          backgroundColor: "#007bff",
+                          borderColor: "#007bff",
+                          cursor: "pointer",
+                          width: "100%",
+                        }}
+                      />
+                    </div>
+                  </div>
+                </form>
               </div>
             </div>
           </div>

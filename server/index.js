742f9f7ed32f88aaa97a04d0b0ec4fbe937cedd8
Maaz <maaz.ahmad21399@gmail.com>
2021-08-07 05:09:42 +0500

    gd

--- server/index.js
+++ server/index.js
@@ -367,7 +367,7 @@ app.get("/coins/:id", async function (req, res) {
   );
 });
 
-app.get("/reacts/:id/:react", auth, async function (req, res) {
+app.get("/reacts/:id/:react", [auth, admin], async function (req, res) {
   let id = req.params.id;
   let react = req.params.react;
   connection.query(`UPDATE coin set ${react} = ${react} + 1 WHERE id = ${id}`);

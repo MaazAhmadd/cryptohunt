77317c150996053cff8d1c1cb6560f1ec2366e6c
Maaz <maaz.ahmad21399@gmail.com>
2021-08-08 22:29:57 +0500

    gdf

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,12 +4,14 @@ export default function footer() {
   return (
     <footer className="footer-main">
       <br />
-      <ul>
-        <li>Disclaimer</li>
-        <li>Privacy Policy</li>
-        <li>Terms &amp; conditions</li>
-        <li>Coins Index</li>
-      </ul>
+      <div style={{ display: "block" }}>
+        <ul>
+          <li>Disclaimer</li>
+          <li>Privacy Policy</li>
+          <li>Terms &amp; conditions</li>
+          <li>Coins Index</li>
+        </ul>
+      </div>
       <img src="" alt="" />
       <img src="" alt="" />
       <p>Cryptohunt © 2021 - contact@cryptohunt.com</p>

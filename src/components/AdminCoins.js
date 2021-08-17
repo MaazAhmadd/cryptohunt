3cfa186ef39b7be9499ae74931c95da8eecec8df
Maaz <maaz.ahmad21399@gmail.com>
2021-08-17 20:53:18 +0500

    fd

--- src/components/AdminCoins.js
+++ src/components/AdminCoins.js
@@ -78,65 +78,70 @@ export default function AdminCoins({ unapprovedCoins }) {
   };
   const manupilatingData = (coins) => {
     let allCoins = [];
-    coins.forEach((coin) => {
-      let dateDiff = Math.ceil(
-        (new Date(coin.launch) -
-          new Date(new Date().toLocaleDateString("en-US"))) /
-          (1000 * 60 * 60 * 24)
-      );
-      let isDatePositive = Math.sign(dateDiff) == "1";
-      let isDateZero = Math.sign(dateDiff) == "0";
-      let change = parseFloat(coin.volume_change_24h).toFixed(2);
-      let isVolumePositive = Math.sign(change) == "1";
-      allCoins.push({
-        id: coin.id,
-        logo: (
-          <img src={coin.logo} style={{ width: "40px", height: "40px" }}></img>
-        ),
-        name: coin.name,
-        volumeChange: Number.isNaN(change) ? (
-          <span>-</span>
-        ) : (
-          <div
-            className={
-              isVolumePositive ? "volume_color_green" : "volume_color_red"
-            }
-          >
-            {isVolumePositive ? <BsCaretUpFill /> : <BsCaretDownFill />}
-            <span>{Math.abs(change)}%</span>
-          </div>
-        ),
-        price: `$${coin.market_cap}`,
-        launch: !isDateZero
-          ? isDatePositive
-            ? `Launching in ${Math.abs(dateDiff)} days`
-            : `Launched ${Math.abs(dateDiff)} days ago`
-          : `Launching Today`,
-        vote: (
-          <>
-            <button
-              onClick={() => reject(coin.id)}
-              title="Vote?"
-              className="admin-table_reject"
+    if (coins) {
+      coins.forEach((coin) => {
+        let dateDiff = Math.ceil(
+          (new Date(coin.launch) -
+            new Date(new Date().toLocaleDateString("en-US"))) /
+            (1000 * 60 * 60 * 24)
+        );
+        let isDatePositive = Math.sign(dateDiff) == "1";
+        let isDateZero = Math.sign(dateDiff) == "0";
+        let change = parseFloat(coin.volume_change_24h).toFixed(2);
+        let isVolumePositive = Math.sign(change) == "1";
+        allCoins.push({
+          id: coin.id,
+          logo: (
+            <img
+              src={coin.logo}
+              style={{ width: "40px", height: "40px" }}
+            ></img>
+          ),
+          name: coin.name,
+          volumeChange: Number.isNaN(change) ? (
+            <span>-</span>
+          ) : (
+            <div
+              className={
+                isVolumePositive ? "volume_color_green" : "volume_color_red"
+              }
             >
-              <BsArrowLeft />
+              {isVolumePositive ? <BsCaretUpFill /> : <BsCaretDownFill />}
+              <span>{Math.abs(change)}%</span>
+            </div>
+          ),
+          price: `$${coin.market_cap}`,
+          launch: !isDateZero
+            ? isDatePositive
+              ? `Launching in ${Math.abs(dateDiff)} days`
+              : `Launched ${Math.abs(dateDiff)} days ago`
+            : `Launching Today`,
+          vote: (
+            <>
+              <button
+                onClick={() => reject(coin.id)}
+                title="Vote?"
+                className="admin-table_reject"
+              >
+                <BsArrowLeft />
+                <span> </span>
+                Reject
+              </button>
               <span> </span>
-              Reject
-            </button>
-            <span> </span>
-            <button
-              onClick={() => approve(coin.id)}
-              title="Vote?"
-              className="admin-table_approve"
-            >
-              Approve
-              <span> </span>
-              <BsArrowRight />
-            </button>
-          </>
-        ),
+              <button
+                onClick={() => approve(coin.id)}
+                title="Vote?"
+                className="admin-table_approve"
+              >
+                Approve
+                <span> </span>
+                <BsArrowRight />
+              </button>
+            </>
+          ),
+        });
       });
-    });
+    }
     return allCoins;
   };
 
